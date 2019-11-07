a404829a326ce2f1b8adf7c1c569948772b82b5e
Javid Mougamadou <javidjms0@gmail.com>
2019-11-07 23:49:12 +0100

    [feat]: Add is_unique_chars()

--- exercises/Cracking-the-Coding-Interview/c1_string.py
+++ exercises/Cracking-the-Coding-Interview/c1_string.py
@@ -9,3 +9,17 @@ def reverse_string(string):
     return arr_string
 
 
+def is_unique_chars(string):
+    if len(string) > 128:
+        return False
+
+    char_set = [False for i in range(128)]
+
+    for char in string:
+        val = ord(char)
+        if char_set[val]:
+            return False
+        char_set[val] = True
+    return True
+
+
